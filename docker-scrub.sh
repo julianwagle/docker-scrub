1c04353fc9a31e6b16b3218e99abef8cebe22f8d
Julian Wagle <54553693+julianwagle@users.noreply.github.com>
2021-11-19 17:05:46 -0600

    SUPER DUPER SCRUBADUBDUB

--- docker-scrub.sh
+++ docker-scrub.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+docker kill $(docker ps -q)
+docker system prune -af
+docker volume prune -f
+docker network prune -f
+docker container ls
+docker image ls
+docker volume ls
+docker network ls
+
+docker kill $(docker ps -q)
+docker system prune -af
+docker volume prune -f
+docker network prune -f
+docker container ls
+docker image ls
+docker volume ls
+docker network ls
+
+docker kill $(docker ps -q)
 docker system prune -af
 docker volume prune -f
 docker network prune -f
